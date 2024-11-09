6b23632659900f7c5b50427ac2d62b492a241c31
engrlukman004 <lukmanabdulsalam11@gmail.com>
2024-11-09 07:42:35 +0100

    first assignment

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# name = input('Hello, please input your name: ')
+# age = input('knowing your Age is not a bad idea? ')
+# location = input('may i know your location: ')
+
+# print(f'Hello {name}, you are {age} year old and live in {location}')
+
+name = input('Hello, please input your name: ')
+age = input('Knowing your age is not a bad idea: ')
+location = input('May I know your location: ')
+
+print(f'Hello {name}, you are {age} years old and live in {location}')
